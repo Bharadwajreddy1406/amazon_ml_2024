284e4405047a53c50332925f47eec9098899dfe3
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2024-09-13 01:35:15 +0530

    comm01: 2

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from src.utils import download_images
+
+if __name__ == '__main__':
+    # Read the CSV file
+    train_df = pd.read_csv(r"dataset/train.csv")
+
+    # Extract the image links
+    image_links = train_df['image_link'].tolist()
+    image_links = image_links[:10]
+
+    # Specify the download folder
+    download_folder = '/downloads'
+
+    # Call the download_images function
+    download_images(image_links, download_folder, allow_multiprocessing=True)
